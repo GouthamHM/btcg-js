9016c7f658375b0a555b1fef97aa6b95fb2fe278
goutham-asu <gmanjuna@asu.edu>
2017-12-21 13:34:19 -0700

    Adding check for countries
    Signed-off-by: goutham-asu <gmanjuna@asu.edu>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
+const countries = require('../data/country_list.json');
+
 function getCurrentPrice(currency) {
+  let found = false;
   if (!currency || typeof currency !== 'string') {
+    return null;
+  }
+  countries.forEach((country) => {
+    if (currency.toLowerCase() === country.currency.toLowerCase()) {
+      found = true;
+    }
+  });
+  if (!found) {
     return null;
   }
   return '$10000';
--- test/index.js
+++ test/index.js
@@ -3,9 +3,11 @@ import getCurrentPrice from '../src/index';
 
 const testSamples = [
   { input: 'USD', expectedResult: '$10000', desc: ' should return $1000' },
+  { input: 'usd', expectedResult: '$10000', desc: ' should return $1000' },
   { input: 'INR', expectedResult: '$10000', desc: ' should return $1000' },
   { input: '', expectedResult: null, desc: ' should return null' },
   { input: 10, expectedResult: null, desc: ' should return null' },
+  { input: 'bala', expectedResult: null, desc: ' should return null' },
 ];
 
 describe('Array', () => {
